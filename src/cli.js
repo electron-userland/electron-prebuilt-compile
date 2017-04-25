e73fe876aa5f0192b52817bcde86da29edcd2f9c
M. Damian Mulligan <robloxianmany@gmail.com>
2017-04-25 15:31:46 -0400

    Forward signals to child process.
    Fixes Electron not exitting when the CLI is spawned as a child process
    itself.

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,9 @@
 import electron from 'electron';
 import proc from 'child_process';
 
+const signalsToForward = ['SIGTERM', 'SIGINT', 'SIGHUP', 'SIGBREAK'];
 let params = [require.resolve('./es6-init')].concat(process.argv.slice(2));
 
 let child = proc.spawn(electron, params, {stdio: 'inherit'});
+signalsToForward.forEach((signal) => process.on(signal, () => child.kill(signal)));
 child.on('close', (code) => process.exit(code));
